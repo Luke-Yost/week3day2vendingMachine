5d336809429c068bf72124a7c611465d074b0bf8
Luke Yost <lukexyost>
2022-05-24 15:08:35 -0600

    initial commit

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
 import { ValuesController } from "./Controllers/ValuesController.js";
 
 class App {
-  valuesController = new ValuesController();
+  // valuesController = new ValuesController();
+  vendItemsController = new Ven
 }
 
 window["app"] = new App();
